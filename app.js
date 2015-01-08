75a5542138ab1682d851b2dc0b15c1c0997f1ecc
Vesa-Matti Hyppönen <vesa.hypponen@gmail.com>
2015-01-08 14:36:14 +0200

    Fixed scope to work like lua in for-loops (just a note to self)

--- app.js
+++ app.js
@@ -7,27 +7,23 @@ var app = function() {
 
     var instance = new object ("application")
 
-    for (var i=1; i <= 10; i++) {
+    for (var i=1; i <= 50; i++) {
+        new function() {
         var rect = instance.add( new rectangle(50,50,{red: Math.random(), green: Math.random(), blue:Math.random()}) )
         rect.identifier = "rectangle "+i
         rect.position.x=getRandom(-screenWidth/2, screenWidth/2)
         rect.position.y=getRandom(-screenHeight/2, screenHeight/2)
         
         rect.spin = function() {
-            console.log("spin "+this.identifier)
             rect.rotate.z = 0
-            rect.anim.animate( rect.rotate, {z:200, time:getRandom(100000,200000), onComplete:
+            rect.anim.animate( rect.rotate, {z:Math.PI*2, time:getRandom(1000,2000), onComplete:
                 function() {
-                    console.log("yes, done "+this.identifier+" : "+rect.rotate.z+" : "+instance.children.length)
-                    //rect.spin()
-
-                    /*for (n=0;n <instance.children.length;n++) {
-                        console.log(instance.children[n].rotate.z)
-                    }*/
+                    rect.spin()
                 }
             })
         }
         rect.spin()
+        }()
     }
 
     instance.add( new image("adCanvas-logo.png") )
