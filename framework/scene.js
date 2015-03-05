062d2668c84678ba0511663d5d5b0178d0e0ef56
Vesa-Matti Hyppönen <vesa.hypponen@gmail.com>
2015-03-05 22:50:54 +0200

    Added damped_orientation to sensors to ease scripting that requires gyros.

--- framework/scene.js
+++ framework/scene.js
@@ -8,15 +8,49 @@
     console.log("initialize scene module")
 
     var animator = curlify.getModule("animator")
+    var sys = curlify.getModule("sys")
 
     var scenestack = []
     var fboUpdateList = []
     var sceneanim = animator.new()
     var pointerStealer = null
 
+    var lastframe = 0
+    var move = function(source,target,inertia,timedelta) {
+      if (timedelta > 100) timedelta = 100
+      var dir = 1
+      if (source > target) dir = -1
+      var move = (-dir*timedelta / (inertia*16.66)) * Math.abs(source-target)
+      if (Math.abs(move) < 0.0001) { source = target } else { source = source-move }
+      if (dir == 1 && source > target) source = target
+      if (dir == -1 && source < target) source = target
+      return source
+    }
+
+    var damp = function(source,target,value,limit,td) {
+      var diff = target[value]-source[value]
+      if (Math.abs(diff) > limit) {
+        var dir = diff / Math.abs(diff)
+        target[value] = move(target[value],source[value]+(dir*360),target.inertia,td)
+        if (dir==-1 && target[value] < -180) target[value] = target[value]+360
+        if (dir==1 && target[value] > 180) target[value] = target[value]-360
+      } else {
+        target[value] = move(target[value],source[value],target.inertia,td)
+      }
+    }
+
     return {
 
       render : function() {
+
+        var td = sys.timestamp()-lastframe
+        lastframe = sys.timestamp()
+
+        damp(curlify.localVars.sensors.orientation,curlify.localVars.sensors.damped_orientation,"alpha",180,td)
+        damp(curlify.localVars.sensors.orientation,curlify.localVars.sensors.damped_orientation,"beta",180,td)
+        damp(curlify.localVars.sensors.orientation,curlify.localVars.sensors.damped_orientation,"gamma",180,td)
+
+        //var ts = sys.timestamp()
         sceneanim.step()
 
         if (sceneanim.animations.length > 0) {
@@ -27,8 +61,12 @@
 
         var target = scenestack[scenestack.length-1]
         if (target == null) return
+
+        //var ts1 = sys.timestamp()
         target.stepTree()
+        //var ts2 = sys.timestamp()
         target.drawTree()
+        //var ts3 = sys.timestamp()
 
         for (var i=0;i<fboUpdateList.length;i++) {
           new function() {
@@ -37,6 +75,9 @@
           }
         }
         fboUpdateList = []
+        //var ts4 = sys.timestamp()
+
+        //console.log((ts1-ts)+","+(ts2-ts1)+","+(ts3-ts2)+","+(ts4-ts3))
 
       },
 
