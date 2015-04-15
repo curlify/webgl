124ad3ad792f4bec4985c24f34914895cdc698cc
Vesa-Matti Hyppönen <vesa.hypponen@gmail.com>
2015-04-15 17:50:24 +0300

    Scenegraph optimisations (minor)

--- framework/quad.js
+++ framework/quad.js
@@ -92,18 +92,20 @@
         instance.quadModelMatrix = mat4.create();
 
         instance.updateModelMatrix = function() {
-          var modelMatrix = this.modelMatrix;
-          mat4.identity(modelMatrix);
+          mat4.identity(this.modelMatrix);
           if (this.position.x != 0 || this.position.y != 0 || this.position.z != 0) {
             var translateScale = screenWidth/(this.translateScale*2)
-            mat4.translate(modelMatrix,modelMatrix,[this.position.x/translateScale,-this.position.y/translateScale,this.position.z/translateScale] );
+            mat4.translate(this.modelMatrix,this.modelMatrix,[this.position.x/translateScale,-this.position.y/translateScale,this.position.z/translateScale] );
           }
-          if (this.rotate.z != 0) mat4.rotateZ(modelMatrix,modelMatrix,-this.rotate.z);
-          if (this.rotate.y != 0) mat4.rotateY(modelMatrix,modelMatrix,-this.rotate.y);
-          if (this.rotate.x != 0) mat4.rotateX(modelMatrix,modelMatrix,-this.rotate.x);
-          if (this.scale.x != 1 || this.scale.y != 1 || this.scale.z != 1) mat4.scale(modelMatrix,modelMatrix,[this.scale.x,this.scale.y,this.scale.z]);
+          if (this.rotate.z != 0) mat4.rotateZ(this.modelMatrix,this.modelMatrix,-this.rotate.z);
+          if (this.rotate.y != 0) mat4.rotateY(this.modelMatrix,this.modelMatrix,-this.rotate.y);
+          if (this.rotate.x != 0) mat4.rotateX(this.modelMatrix,this.modelMatrix,-this.rotate.x);
+          if (this.scale.x != 1 || this.scale.y != 1 || this.scale.z != 1) mat4.scale(this.modelMatrix,this.modelMatrix,[this.scale.x,this.scale.y,this.scale.z]);
+          if (this.origo.x != 0 || this.origo.y != 0 || this.origo.z != 0) {
+            var translateScale = screenWidth/(this.translateScale*2)
+            mat4.translate(this.modelMatrix,this.modelMatrix,vec3.clone([-this.origo.x/translateScale,this.origo.y/translateScale,-this.origo.z/translateScale]) );
+          }
 
-          this.modelMatrix = modelMatrix;
           mat4.copy(this.quadModelMatrix,this.modelMatrix);
 
           var multiplier = screenWidth/this.translateScale;
